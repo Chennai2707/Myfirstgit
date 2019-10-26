905f7b12108e9cc065866029f1f9ead2099b9e0f
s.badrinath <s.badrinath@WKSBAN24KAN0019.corp.mphasis.com>
2019-10-26 09:59:56 +0530

    Main class

---
+++ Myfirst/src/com/mphasis/Test.java
@@ -0,0 +1,15 @@
+package com.mphasis;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		Account a= new Account(123, "badri");
+		String str =a.getAcname();
+		int b=a.getAcno();
+		System.out.println(b);
+		System.out.println(str);
+	}
+
+}
