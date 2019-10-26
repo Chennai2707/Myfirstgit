5a834b9b7220bddce5a3541d4a9f1e10f5e214ce
s.badrinath <s.badrinath@WKSBAN24KAN0019.corp.mphasis.com>
2019-10-26 10:15:10 +0530

    commited

--- Myfirst/src/com/mphasis/Test.java
+++ Myfirst/src/com/mphasis/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		//badri here
+		//badri here anjon
 		Account a= new Account(123, "badri");
 		String str =a.getAcname();
 		int b=a.getAcno();
